3944ce29bb83b15071e900f1fb2fefeca5539775
Matheus Garay Trindade <mtrindade@inf.ufsm.br>
2016-11-02 19:35:06 -0200

    primeiro commit

---
+++ src/Testes.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.fbergeron.card.CardTest;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({CardTest.class})
+public class Testes {
+
+}
---
+++ src/com/fbergeron/card/CardTest.java
@@ -0,0 +1,19 @@
+package com.fbergeron.card;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void test() {
+		assertEquals(0, 1);
+	}
+	
+	@Test
+	public void testLala(){
+		assertEquals(1, 1);
+	}
+
+}
